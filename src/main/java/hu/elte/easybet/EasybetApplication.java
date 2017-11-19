32eeda39850c7137ddf42d31b0f6af4bfde1d39d
SlimSider <33596549+SlimSider@users.noreply.github.com>
2017-11-19 15:18:37 +0100

    initial project commit

---
+++ src/main/java/hu/elte/easybet/EasybetApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.easybet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybetApplication.class, args);
+	}
+}
