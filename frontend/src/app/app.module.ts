3ca23a83052c67a27ad8fa8dbb457e52a8d609ee
Mertinousse <mertinousse@gmail.com>
2017-12-10 22:37:08 +0100

    removed bad test files, added a working test for match.service
    there is still one not resolved test

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router'
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {LoginComponent} from './pages/login/login.component';
--- frontend/src/app/services/match.service.spec.ts
+++ frontend/src/app/services/match.service.spec.ts
@@ -1,15 +1,36 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { HttpClient } from '@angular/common/http';
+import {HttpModule} from "@angular/http";
 
 import { MatchService } from './match.service';
+import { Match } from '../model/Match';
 
 describe('IssueService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MatchService]
+      providers: [MatchService, {provide: HttpClient, deps: [MockBackend]},],
+      imports: [
+        HttpModule
+      ],
     });
   });
 
   it('should be created', inject([MatchService], (service: MatchService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should create the given match', inject([MatchService], (service: MatchService) => {
+    service
+      .getMatches()
+      .subscribe(matches => {
+        const match = new Match("home", "away", true, 1, 1, 0, 0, null, 10);
+
+        service.create(match);
+
+        expect(matches.length).toBe(1);
+        expect(matches[0]).toBe(match);
+      });
+  }));
+
 });
