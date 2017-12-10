f94bb2be919088a0ecf8ea9b80675e63b495f787
Mertinousse <mertinousse@gmail.com>
2017-12-10 23:28:51 +0100

    added an e2e test on creating a match

--- frontend/e2e/app.po.ts
+++ frontend/e2e/app.po.ts
@@ -5,7 +5,32 @@ export class AppPage {
     return browser.get('/');
   }
 
+  navigateToMatches() {
+    return browser.get('/match');
+  }
+
+  navigateToNewMatch() {
+    return browser.get('/match/new');
+  }
+
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getHomeInput() {
+    return element(by.css('div.form-control:nth-child(1) input'));
+  }
+
+  getAwayInput() {
+    return element(by.css('div.form-control:nth-child(2) input'));
+  }
+
+  getCreateButton() {
+    return element(by.css('form > button'));
+  }
+
+  getMatchCount() {
+    return element.all(by.css('mat-row')).count();
+  }
+
 }
